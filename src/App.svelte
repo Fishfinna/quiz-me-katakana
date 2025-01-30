60c144895ee0fab232eb7b1bcd9a6a2370c132fa
Shanti <fishfinna12@gmail.com>
2025-01-29 20:28:55 -0800

    added the json data

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
@@ -14,6 +15,8 @@
   </div>
   <h1>Vite + Svelte</h1>
 
+  <Counter name="test" />
+
   <p>
     Check out <a
       href="https://github.com/sveltejs/kit#readme"
@@ -25,20 +28,5 @@
   <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+<style lang="scss">
 </style>
--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  let count: number = $state(0)
+  let props = $props();
+  let count: number = $state(0);
   const increment = () => {
-    count += 1
-  }
+    count += 1;
+  };
 </script>
 
 <button onclick={increment}>
   count is {count}
+  {props.name}
 </button>
