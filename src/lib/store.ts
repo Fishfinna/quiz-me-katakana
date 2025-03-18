023213b9c3199d7fbc05d5ad76e67b6bd4178629
Shanti <fishfinna12@gmail.com>
2025-03-17 17:51:45 -0700

    saved to long term storage

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,13 +1,26 @@
 import { writable } from "svelte/store";
 
-export const attempts = writable(3);
-export const font = writable("mono");
-export const isHiragana = writable(false);
-export const isDarkMode = writable(false);
-export const mute = writable(false);
-export const displayScore = writable(false);
-export const characterFilter = writable({
+function createSessionWritable(key: string, initialValue: any) {
+  const storedValue = sessionStorage.getItem(key);
+  const value = storedValue ? JSON.parse(storedValue) : initialValue;
+
+  const store = writable(value);
+
+  store.subscribe(($store) => {
+    sessionStorage.setItem(key, JSON.stringify($store));
+  });
+
+  return store;
+}
+
+export const attempts = createSessionWritable("attempts", 3);
+export const font = createSessionWritable("font", "mono");
+export const isHiragana = createSessionWritable("isHiragana", false);
+export const isDarkMode = createSessionWritable("isDarkMode", false);
+export const mute = createSessionWritable("mute", false);
+export const displayScore = createSessionWritable("displayScore", false);
+export const characterFilter = createSessionWritable("characterFilter", {
   standard: true,
   voiced: false,
   compound: false,
-} as Record<string, boolean>);
+});
