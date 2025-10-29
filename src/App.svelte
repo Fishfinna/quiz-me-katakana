fd1332c39f66d897d85eb804ce37d72ce5ff4b1b
Shanti <fishfinna12@gmail.com>
2025-10-28 17:17:04 -0700

    input set up and texbox moved

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,13 @@
   import Router from "svelte-spa-router";
   import Navigation from "./lib/nav/Navigation.svelte";
   import Home from "./pages/game/Game.svelte";
+  import Keyboard from "./lib/keyboard/Keyboard.svelte";
   import Learn from "./pages/learn/Learn.svelte";
 
   const routes = {
     "/": Home,
     "/learn": Learn,
-    "/keyboard": Home,
+    "/keyboard": Keyboard,
     "*": Home,
   };
 </script>
--- src/lib/keyboard/Keyboard.svelte
+++ src/lib/keyboard/Keyboard.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
   import katakana from "../../assets/data/katakana.json";
   import hiragana from "../../assets/data/hiragana.json";
+  let userInput: string;
+  let inputElement: HTMLElement;
 </script>
 
 <div>
   <h1>Keyboard</h1>
+  <input
+    type="text"
+    bind:value={userInput}
+    bind:this={inputElement}
+    autocorrect="off"
+  />
 </div>
